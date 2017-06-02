b0e7701ef3e300fdcb4cff97931494df4d62aeb3
i3arnon <i3arnon@gmail.com>
2017-06-02 20:15:05 +0300

    Add CancellableTaskCompletionSource

---
+++ src/AsyncUtilities/CancellableTaskCompletionSource.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncUtilities
+{
+    public class CancellableTaskCompletionSource<TResult> : TaskCompletionSource<TResult>
+    {
+        private CancellationTokenRegistration _registration;
+
+        public CancellationToken CancellationToken { get; }
+
+        public CancellableTaskCompletionSource(CancellationToken cancellationToken)
+            : this(cancellationToken, null, TaskCreationOptions.None)
+        {
+        }
+
+        public CancellableTaskCompletionSource(CancellationToken cancellationToken, TaskCreationOptions creationOptions)
+            : this(cancellationToken, null, creationOptions)
+        {
+        }
+
+        public CancellableTaskCompletionSource(CancellationToken cancellationToken, object state)
+            : this(cancellationToken, state, TaskCreationOptions.None)
+        {
+        }
+
+        public CancellableTaskCompletionSource(
+            CancellationToken cancellationToken,
+            object state,
+            TaskCreationOptions creationOptions)
+            : base(state, creationOptions)
+        {
+            CancellationToken = cancellationToken;
+
+            _registration = cancellationToken.Register(
+                @this => ((CancellableTaskCompletionSource<TResult>)@this).TrySetCanceled(),
+                this);
+
+            Task.ContinueWith(
+                (_, @this) => ((CancellableTaskCompletionSource<TResult>)@this)._registration.Dispose(),
+                this,
+                CancellationToken.None,
+                TaskContinuationOptions.ExecuteSynchronously,
+                TaskScheduler.Default);
+        }
+    }
+}
