5ea850754ae08fadc116b8348a483e4015dbdb9f
i3arnon <i3arnon@gmail.com>
2017-06-02 21:52:48 +0300

    Add xml comments

--- src/AsyncUtilities/CancellableTaskCompletionSource.cs
+++ src/AsyncUtilities/CancellableTaskCompletionSource.cs
@@ -3,27 +3,74 @@ using System.Threading.Tasks;
 
 namespace AsyncUtilities
 {
+    /// <summary>
+    /// Represents a <see cref="TaskCompletionSource{TResult}"/> associated with a <see cref="CancellationToken"/>.
+    /// Canceling the <see cref="CancellationToken"/> will cancel the <see cref="CancellableTaskCompletionSource{TResult}"/>.
+    /// </summary>
+    /// <typeparam name="TResult">
+    /// The type of the result value associated with this <see cref="CancellableTaskCompletionSource{TResult}"/>.
+    /// </typeparam>
     public class CancellableTaskCompletionSource<TResult> : TaskCompletionSource<TResult>
     {
         private CancellationTokenRegistration _registration;
 
+        /// <summary>
+        /// Gets the <see cref="CancellationToken"/> associated with this <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </summary>
         public CancellationToken CancellationToken { get; }
 
+        /// <summary>
+        /// Creates a <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/> to associate with the <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </param>
         public CancellableTaskCompletionSource(CancellationToken cancellationToken)
             : this(cancellationToken, null, TaskCreationOptions.None)
         {
         }
 
+        /// <summary>
+        /// Creates a <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/> to associate with the <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </param>
+        /// <param name="creationOptions">
+        /// The options to use when creating the underlying <see cref="Task{TResult}"/>.
+        /// </param>
         public CancellableTaskCompletionSource(CancellationToken cancellationToken, TaskCreationOptions creationOptions)
             : this(cancellationToken, null, creationOptions)
         {
         }
 
+
+        /// <summary>
+        /// Creates a <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/> to associate with the <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </param>
+        /// <param name="state">
+        /// The state to use as the underlying <see cref="Task{TResult}"/>'s AsyncState.
+        /// </param>
         public CancellableTaskCompletionSource(CancellationToken cancellationToken, object state)
             : this(cancellationToken, state, TaskCreationOptions.None)
         {
         }
 
+        /// <summary>
+        /// Creates a <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/> to associate with the <see cref="CancellableTaskCompletionSource{TResult}"/>.
+        /// </param>
+        /// <param name="state">
+        /// The state to use as the underlying <see cref="Task{TResult}"/>'s AsyncState.
+        /// </param>
+        /// <param name="creationOptions">
+        /// The options to use when creating the underlying <see cref="Task{TResult}"/>.
+        /// </param>
         public CancellableTaskCompletionSource(
             CancellationToken cancellationToken,
             object state,
--- src/AsyncUtilities/TaskExtensions.cs
+++ src/AsyncUtilities/TaskExtensions.cs
@@ -4,8 +4,26 @@ using System.Threading.Tasks;
 
 namespace AsyncUtilities
 {
+    /// <summary>Provides a set of static methods for working with specific kinds of <see cref="Task"/> instances.</summary>
     public static class TaskExtensions
     {
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task"/>.
+        /// </param>
+        /// <param name="continuationAction">
+        /// An action to run when the <see cref="Task"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationAction"/> argument is null.
+        /// </exception>
         public static Task ContinueWithSynchronously(this Task task, Action<Task> continuationAction)
         {
             if (task == null) throw new ArgumentNullException(nameof(task));
@@ -18,6 +36,26 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task"/>.
+        /// </param>
+        /// <param name="continuationAction">
+        /// An action to run when the <see cref="Task"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <param name="state">
+        /// An object representing data to be used by the continuation action.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationAction"/> argument is null.
+        /// </exception>
         public static Task ContinueWithSynchronously(
             this Task task,
             Action<Task, object> continuationAction,
@@ -34,6 +72,23 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task"/>.
+        /// </param>
+        /// <param name="continuationFunction">
+        /// A function to run when the <see cref="Task"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task{TResult}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationFunction"/> argument is null.
+        /// </exception>
         public static Task<TResult> ContinueWithSynchronously<TResult>(
             this Task task,
             Func<Task, TResult> continuationFunction)
@@ -48,6 +103,26 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task"/>.
+        /// </param>
+        /// <param name="continuationFunction">
+        /// A function to run when the <see cref="Task"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <param name="state">
+        /// An object representing data to be used by the continuation function.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task{TResult}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationFunction"/> argument is null.
+        /// </exception>
         public static Task<TResult> ContinueWithSynchronously<TResult>(
             this Task task,
             Func<Task, object, TResult> continuationFunction,
@@ -64,6 +139,23 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task{TResult}"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task{TResult}"/>.
+        /// </param>
+        /// <param name="continuationAction">
+        /// An action to run when the <see cref="Task{TResult}"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationAction"/> argument is null.
+        /// </exception>
         public static Task ContinueWithSynchronously<TResult>(
             this Task<TResult> task,
             Action<Task<TResult>> continuationAction)
@@ -78,6 +170,26 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task{TResult}"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task{TResult}"/>.
+        /// </param>
+        /// <param name="continuationAction">
+        /// An action to run when the <see cref="Task{TResult}"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <param name="state">
+        /// An object representing data to be used by the continuation action.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationAction"/> argument is null.
+        /// </exception>
         public static Task ContinueWithSynchronously<TResult>(
             this Task<TResult> task,
             Action<Task<TResult>, object> continuationAction,
@@ -94,6 +206,23 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task{TResult}"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task{TResult}"/>.
+        /// </param>
+        /// <param name="continuationFunction">
+        /// A function to run when the <see cref="Task{TResult}"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task{TNewResult}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationFunction"/> argument is null.
+        /// </exception>
         public static Task<TNewResult> ContinueWithSynchronously<TResult, TNewResult>(
             this Task<TResult> task,
             Func<Task<TResult>, TNewResult> continuationFunction)
@@ -108,6 +237,26 @@ namespace AsyncUtilities
                 TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Creates a continuation that executes synchronously on the ThreadPool when the target <see cref="Task{TResult}"/> completes.
+        /// </summary>
+        /// <param name="task">
+        /// The antecedent <see cref="Task{TResult}"/>.
+        /// </param>
+        /// <param name="continuationFunction">
+        /// A function to run when the <see cref="Task{TResult}"/> completes. When run, the delegate will be
+        /// passed the completed task as an argument.
+        /// </param>
+        /// <param name="state">
+        /// An object representing data to be used by the continuation function.
+        /// </param>
+        /// <returns>A new continuation <see cref="Task{TNewResult}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="task"/> argument is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="continuationFunction"/> argument is null.
+        /// </exception>
         public static Task<TNewResult> ContinueWithSynchronously<TResult, TNewResult>(
             this Task<TResult> task,
             Func<Task<TResult>, object, TNewResult> continuationFunction,
